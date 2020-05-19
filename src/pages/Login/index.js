767c3a13541825b589713ca1d5c0a0102f7b83d6
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-05-19 17:50:07 -0300

    add página de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 
 import api from '../../services/api'
+import './styles.css'
+
+import iceCream from '../../assets/iceCream.svg'
+import logo from '../../assets/logo.svg'
 
 export default function Login() {
 
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="logon-container">
+            <span id="text-background">SCOOPS AHOY</span>
+            <img className="cream-image" src={iceCream} alt="Heroes" />
+
+            <section className="form">
+                <img src={logo} alt="Scoops Ahoy"/>
+
+                <form >
+                    <h1>Login do administrador</h1>
+
+                    <input 
+                        placeholder="Senha"
+        
+                    />
+                    <button className="button" type="submit" >Entrar</button>
+
+                    
+                </form>
+
+            </section>
+    
         </div>
     )
 }
