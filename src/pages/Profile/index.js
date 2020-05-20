22e9e4de34c4a4d4c06501fb5acd90396ad87d85
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-05-20 17:12:28 -0300

    roteamento login e inicia page profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiPower } from 'react-icons/fi'
 
 import './styles.css'
 import api from '../../services/api'
 
+import logoImg from '../../assets/logo.svg'
+
 export default function Profile() {
+
+    const history = useHistory()
+
+    function handleLogout() {
+        history.push('/');
+    }
 
 
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="profile-container">
+            
+            <header>
+                <img src={logoImg} alt="Scoops Ahoy"/>
+            
+                <Link className="button" to="/incidents/new">Cadastrar novo sorvete</Link>
+                <button onClick={handleLogout} type="button">
+                    <FiPower size="18" color="#e02041" />
+                </button>
+            </header>
+
+            <div className="menu">
+                <span>Pedidos</span>
+                <span>Histórico</span>
+            </div>
+
         </div>
     )
 }
